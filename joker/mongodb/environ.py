ec523cbbe5e48d249502991fc4c14277d2ea7782
frozflame <frozflame@outlook.com>
2020-01-01 08:22:37 +0800

    GIMixinMongo

--- joker/mongodb/environ.py
+++ joker/mongodb/environ.py
@@ -3,30 +3,41 @@
 
 import abc
 
+from gridfs import GridFS
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
+from volkanic.compat import cached_property
 
 from joker.mongodb.wrappers import DatabaseWrapper, CollectionWrapper
 
 
-class MultihostMixin(abc.ABC):
-    _mongo_clients = {}
-
-    def _get_mongo(self, host: str) -> MongoClient:
-        conf = getattr(self, 'conf', {'mongo_hosts': {}})
-        hosts = conf['mongo_hosts']
+class MongoInterface:
+    def __init__(self, hosts: dict = None, defaults: list = None):
+        if hosts is None:
+            hosts = {}
         hosts.setdefault('lh', {})
+        self._clients = {}
+        self.hosts = hosts
+        self.defaults = defaults or ['lh', 'test']
+
+    def get_mongo(self, host: str = None) -> MongoClient:
+        if host is None:
+            host = self.defaults[0]
         try:
-            return self._mongo_clients[host]
+            return self._clients[host]
         except KeyError:
-            params = hosts.get(host, {'host': host})
+            params = self.hosts.get(host, {'host': host})
             mongo = MongoClient(**params)
-            self._mongo_clients[host] = mongo
+            self._clients[host] = mongo
             return mongo
 
+    @property
+    def db(self):
+        return self.get_db(*self.defaults)
+
     def get_db(self, host: str, db_name: str) -> Database:
-        mongo = self._get_mongo(host)
+        mongo = self.get_mongo(host)
         return mongo.get_database(db_name)
 
     def get_dbw(self, host: str, db_name: str):
@@ -40,3 +51,21 @@ class MultihostMixin(abc.ABC):
     def get_collw(self, host: str, db_name, coll_name) -> CollectionWrapper:
         coll = self.get_coll(host, db_name, coll_name)
         return CollectionWrapper(coll)
+
+    def get_gridfs(self, host: str, db_name, coll_name='fs') -> GridFS:
+        # avoid names like "images.files.files"
+        if coll_name.endswith('.files') or coll_name.endswith('.chunks'):
+            coll_name = coll_name.rsplit('.', 1)[0]
+        db = self.get_db(host, db_name)
+        return GridFS(db, collection=coll_name)
+
+
+class GIMixinMongo:
+    @cached_property
+    def mongoi(self) -> MongoInterface:
+        conf = getattr(self, 'conf', {})
+        return MongoInterface(conf.get('mongo_hosts'))
+
+    @cached_property
+    def mongo(self) -> MongoClient:
+        return self.mongoi.get_mongo()
