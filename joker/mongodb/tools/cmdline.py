07bc60afde9bbddd0eaf3846ba99edeb3640199f
frozflame <frozflame@outlook.com>
2022-01-20 20:30:01 +0800

    ArgumentParserMongoish.add_arguments_host_port, arg hostname

--- joker/mongodb/tools/cmdline.py
+++ joker/mongodb/tools/cmdline.py
@@ -8,9 +8,9 @@ class ArgumentParserMongoish(argparse.ArgumentParser):
     def add_argument_mongoi_host(self):
         self.add_argument('-H', '--host', help='host as in MongoInterface')
 
-    def add_arguments_host_port(self):
+    def add_arguments_host_port(self, hostname='127.0.0.1'):
         add = self.add_argument
-        add('-H', '--hostname', default='127.0.0.1', help='hostname')
+        add('-H', '--hostname', default=hostname, help='hostname')
         add('-p', '--port', type=int, default=27017, help='port number')
 
     def add_argument_db_name(self):
