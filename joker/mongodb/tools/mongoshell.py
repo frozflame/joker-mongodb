950ecd9f5f07265b5399094dbd050b8feb20d15d
frozflame <frozflame@outlook.com>
2022-02-24 21:25:47 +0800

    inplace_fix_mongoshell_jsonfile: check if result json is valid

--- joker/mongodb/tools/mongoshell.py
+++ joker/mongodb/tools/mongoshell.py
@@ -2,12 +2,14 @@
 # coding: utf-8
 
 import re
+import json
 
 from bson.json_util import loads
 import datetime
 
 
 def fix_mongoshell_json(s: str):
+    # https://stackoverflow.com/a/11886476/2925169
     s = re.sub(
         r':\s*ObjectId\s*\(\s*\"(\S+)\"\s*\)',
         r': {"$oid": "\1"}', s
@@ -23,16 +25,21 @@ def loads_mongoshell_json(s: str):
     return loads(fix_mongoshell_json(s))
 
 
-def inplace_fix_mongoshell_jsonfile(path: str):
+def inplace_fix_mongoshell_jsonfile(path: str) -> bool:
     now = datetime.datetime.now()
     raw = open(path).read()
     fixed = fix_mongoshell_json(raw)
     if raw == fixed:
-        return
+        return False
+    try:
+        json.loads(fixed)
+    except json.JSONDecodeError:
+        return False
     with open(path + f'.{now:%Y%m%d-%H%M%S}.bak', 'w') as fout:
         fout.write(raw)
     with open(path, 'w') as fout:
         fout.write(fixed)
+    return True
 
 
 def main():
