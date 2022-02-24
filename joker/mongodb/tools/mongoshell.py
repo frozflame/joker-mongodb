bf3abb80118103a4a02d762ea7e033d9da5922db
frozflame <frozflame@outlook.com>
2022-02-24 20:58:24 +0800

    inplace_fix_mongoshell_jsonfile()

--- joker/mongodb/tools/mongoshell.py
+++ joker/mongodb/tools/mongoshell.py
@@ -4,6 +4,7 @@
 import re
 
 from bson.json_util import loads
+import datetime
 
 
 def fix_mongoshell_json(s: str):
@@ -22,11 +23,21 @@ def loads_mongoshell_json(s: str):
     return loads(fix_mongoshell_json(s))
 
 
+def inplace_fix_mongoshell_jsonfile(path: str):
+    now = datetime.datetime.now()
+    raw = open(path).read()
+    fixed = fix_mongoshell_json(raw)
+    if raw == fixed:
+        return
+    with open(path + f'.{now:%Y%m%d-%H%M%S}.bak', 'w') as fout:
+        fout.write(raw)
+    with open(path, 'w') as fout:
+        fout.write(fixed)
+
+
 def main():
     import sys
-    s = fix_mongoshell_json(sys.stdin.read())
-    print(fix_mongoshell_json(s))
-    print(loads_mongoshell_json(s))
+    inplace_fix_mongoshell_jsonfile(sys.argv[1])
 
 
 if __name__ == '__main__':
